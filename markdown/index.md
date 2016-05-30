d9c93c232b62bea26ab51b13bd2581a16a58c0e1
Libby Horacek <libby@daydrea.me>
2016-05-30 12:31:29 -0400

    Rearrange personal pages

---
+++ markdown/index.md
@@ -0,0 +1,1 @@
+This is a mostly-empty index page!
--- site-generator/site-generator.hs
+++ site-generator/site-generator.hs
@@ -1,24 +1,35 @@
 {-# LANGUAGE OverloadedStrings #-}
+import           Data.List       (isPrefixOf)
 import           Debug.Trace
 import           Hakyll
+import           System.FilePath
 
 main :: IO ()
 main = hakyllWith conf $ do
+
     match "images/*" $ do
         route   idRoute
         compile copyFileCompiler
 
-    match "css/*" $ do
-        route   idRoute
-        compile compressCssCompiler
-
-    match "*.md" $ trace "found" $ do
+    match "markdown/*.md" $ do
         route   $ setExtension "html"
         compile $ pandocCompiler
             >>= loadAndApplyTemplate "templates/default.html" defaultContext
             >>= relativizeUrls
 
+    match "create-your-personal-page-here/*" $ do
+      route $ (gsubRoute "create-your-personal-page-here" (const "personal")) `composeRoutes` (setExtension "html")
+      compile $ pandocCompiler
+        >>= loadAndApplyTemplate "templates/personal.html" defaultContext
+        >>= relativizeUrls
+
     match "templates/*" $ compile templateCompiler
 
 conf = defaultConfiguration { providerDirectory = "current"
+                            , ignoreFile = ignoreFile'
                             , deployCommand = "mv _site/* ../"}
+       where  ignoreFile' path
+                | "current" `isPrefixOf` fileName = True
+                | otherwise                       = False
+                where
+                  fileName = takeFileName path
